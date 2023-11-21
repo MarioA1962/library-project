d0f2ddad44a48d47515864a27f91b00478e7ee41
Christian Casas <christian.casas.m@uni.pe>
2023-11-21 12:02:35 -0500

    sql actualizzz

--- libreria.sql
+++ libreria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-11-2023 a las 17:25:34
+-- Tiempo de generación: 21-11-2023 a las 18:01:11
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.0.28
 
@@ -20,6 +20,67 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `libreria`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `autores`
+--
+
+CREATE TABLE `autores` (
+  `id` int(11) NOT NULL,
+  `nombre` text NOT NULL,
+  `bibliografia` text NOT NULL,
+  `foto` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `autores`
+--
+
+INSERT INTO `autores` (`id`, `nombre`, `bibliografia`, `foto`) VALUES
+(1, 'Stephen King', 'Stephen Edwin King, más conocido como Stephen King y ocasionalmente por su pseudónimo Richard Bachman, es un escritor estadounidense de novelas de terror, ficción sobrenatural, misterio, ciencia ficción y literatura fantástica.', 'Vistas/img/Autores/A-499.jpg'),
+(2, 'Cesar Vallejo', 'César Abraham Vallejo Mendoza fue un poeta y escritor peruano. Es considerado uno de los mayores innovadores de la poesía universal del siglo XX y el máximo exponente de las letras en Perú.', 'Vistas/img/Autores/A-358.jpg'),
+(3, 'Gabriel García Márquez', 'Gabriel José de la Concordia García Márquez ​​ fue un escritor y periodista colombiano. Reconocido principalmente por sus novelas y cuentos, también escribió narrativa de no ficción, discursos, reportajes, críticas cinematográficas y memorias.', 'Vistas/img/Autores/A-144.jpg'),
+(6, 'Abraham Valdelomar', 'Pedro Abraham Valdelomar Pinto, también mencionado como el Conde de Lemos, fue un narrador, poeta, periodista, dibujante, ensayista y dramaturgo peruano. Está considerado uno de los principales cuentistas del Perú, junto con Julio Ramón Ribeyro.', 'Vistas/img/Autores/A-911.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `id` int(11) NOT NULL,
+  `nombre` text NOT NULL,
+  `apellido` text NOT NULL,
+  `documento` text NOT NULL,
+  `fecha_nac` text NOT NULL,
+  `direccion` text NOT NULL,
+  `telefono` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `generos`
+--
+
+CREATE TABLE `generos` (
+  `id` int(11) NOT NULL,
+  `nombre` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `generos`
+--
+
+INSERT INTO `generos` (`id`, `nombre`) VALUES
+(1, 'Novela'),
+(2, 'Terror'),
+(3, 'Ficción'),
+(9, 'Poesía'),
+(10, 'Fantasía');
 
 -- --------------------------------------------------------
 
@@ -43,11 +104,30 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `usuario`, `clave`, `nombre`, `apellido`, `documento`, `foto`, `rol`) VALUES
-(1, 'admin', 'admin', 'Oscar Medina', 'Alvarado Torres', '62374376', 'Vistas/img/Usuarios/U-562.png', 'Administrador');
+(1, 'admin', 'admin', 'Oscar Medina', 'Alvarado Torres', '62374376', 'Vistas/img/Usuarios/U-694.png', 'Administrador'),
+(4, 'jeanc', 'jeanc', 'Jorge', 'Carhuas Chirinos', '73942812', 'Vistas/img/Usuarios/U-864.jpg', 'Vendedor');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `autores`
+--
+ALTER TABLE `autores`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `generos`
+--
+ALTER TABLE `generos`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -60,10 +140,28 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `autores`
+--
+ALTER TABLE `autores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `generos`
+--
+ALTER TABLE `generos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
