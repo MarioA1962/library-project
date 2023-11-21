145a8024bd8431b8347f3907af14926d4e2273a0
Christian Casas <christian.casas.m@uni.pe>
2023-11-21 16:07:08 -0500

    sql 2.0

--- libreria.sql
+++ libreria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 21-11-2023 a las 18:01:11
+-- Tiempo de generación: 21-11-2023 a las 22:05:42
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.0.28
 
@@ -42,7 +42,7 @@ INSERT INTO `autores` (`id`, `nombre`, `bibliografia`, `foto`) VALUES
 (1, 'Stephen King', 'Stephen Edwin King, más conocido como Stephen King y ocasionalmente por su pseudónimo Richard Bachman, es un escritor estadounidense de novelas de terror, ficción sobrenatural, misterio, ciencia ficción y literatura fantástica.', 'Vistas/img/Autores/A-499.jpg'),
 (2, 'Cesar Vallejo', 'César Abraham Vallejo Mendoza fue un poeta y escritor peruano. Es considerado uno de los mayores innovadores de la poesía universal del siglo XX y el máximo exponente de las letras en Perú.', 'Vistas/img/Autores/A-358.jpg'),
 (3, 'Gabriel García Márquez', 'Gabriel José de la Concordia García Márquez ​​ fue un escritor y periodista colombiano. Reconocido principalmente por sus novelas y cuentos, también escribió narrativa de no ficción, discursos, reportajes, críticas cinematográficas y memorias.', 'Vistas/img/Autores/A-144.jpg'),
-(6, 'Abraham Valdelomar', 'Pedro Abraham Valdelomar Pinto, también mencionado como el Conde de Lemos, fue un narrador, poeta, periodista, dibujante, ensayista y dramaturgo peruano. Está considerado uno de los principales cuentistas del Perú, junto con Julio Ramón Ribeyro.', 'Vistas/img/Autores/A-911.jpg');
+(8, 'Abraham Valdelomar', 'Pedro Abraham Valdelomar Pinto, también mencionado como el Conde de Lemos, fue un narrador, poeta, periodista, dibujante, ensayista y dramaturgo peruano. Está considerado uno de los principales cuentistas del Perú, junto con Julio Ramón Ribeyro.', 'Vistas/img/Autores/A-131.jpg');
 
 -- --------------------------------------------------------
 
@@ -59,6 +59,13 @@ CREATE TABLE `clientes` (
   `direccion` text NOT NULL,
   `telefono` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`id`, `nombre`, `apellido`, `documento`, `fecha_nac`, `direccion`, `telefono`) VALUES
+(1, 'Christian Joel', 'Casas Moreno', '20202094', '07/02/2001', 'Comas City, Lima, Perú', '964338663');
 
 -- --------------------------------------------------------
 
@@ -81,6 +88,32 @@ INSERT INTO `generos` (`id`, `nombre`) VALUES
 (3, 'Ficción'),
 (9, 'Poesía'),
 (10, 'Fantasía');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `libros`
+--
+
+CREATE TABLE `libros` (
+  `id` int(11) NOT NULL,
+  `titulo` text NOT NULL,
+  `id_genero` int(11) NOT NULL,
+  `id_autor` int(11) NOT NULL,
+  `sinopsis` text NOT NULL,
+  `portada` text NOT NULL,
+  `idioma` text NOT NULL,
+  `fecha_publicacion` text NOT NULL,
+  `precio` text NOT NULL,
+  `stock` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `libros`
+--
+
+INSERT INTO `libros` (`id`, `titulo`, `id_genero`, `id_autor`, `sinopsis`, `portada`, `idioma`, `fecha_publicacion`, `precio`, `stock`) VALUES
+(3, 'IT', 2, 1, 'Tras veintisiete años de tranquilidad y lejanía, una antigua promesa infantil les hace volver al lugar en el que vivieron su infancia y juventud como una terrible pesadilla. Regresan a Derry para enfrentarse con su pasado y enterrar definitivamente la amenaza que los amargó durante su niñez.', 'Vistas/img/Libros/Portada-151.jpg', 'Español', '04/07/1990', '175', '7');
 
 -- --------------------------------------------------------
 
@@ -130,6 +163,12 @@ ALTER TABLE `generos`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `libros`
+--
+ALTER TABLE `libros`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
@@ -143,19 +182,25 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `autores`
 --
 ALTER TABLE `autores`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `generos`
 --
 ALTER TABLE `generos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de la tabla `libros`
+--
+ALTER TABLE `libros`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
